3d654600b197837270b36961ed436d754ce2ef96
ayoubxid <oussamahassania87@gmail.com>
2025-11-02 12:25:16 +0100

    i add some algorithmes

--- src/Test.java
+++ src/Test.java
@@ -62,6 +62,67 @@ public class Test {
          * else
          *     System.out.println("Element not found");
          */
+        int []array={8,9,7,6,5,4,2,3,1};
+        mergeSort(array);
+        for (int i=0;i<array.length;i++) {
+            System.out.print(array[i]+" ");
+        }
+    }
+    /* ============================================================
+     *                   Merge sort
+     * ============================================================
+     *
+     * Merge Sort is an efficient, comparison-based sorting algorithm that employs the divide and conquer strategy.
+     * It was invented by John von Neumann in 1945.
+     *
+     * run-time complexity O(n log(n))
+     * space complexity O(n)
+     */
+    private static void mergeSort(int[] array) {
+        int length=array.length;
+        if (length<=1)return;//base case
+        int middle = length/2;
+        int []leftArray=new int[middle];
+        int []rightArray=new int[length-middle];
+        int i=0;
+        int j=0;
+        for(;i<length;i++){
+            if (i<middle) {
+                leftArray[i]=array[i];
+            }else{
+                rightArray[j]=array[i];
+                j++;
+            }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray,rightArray,array);
+    }
+    private static void merge( int[] leftArray,int[]rightArray,int[]array) {
+         int leftSize=array.length/2;
+         int rightSize=array.length-leftSize;
+         int i=0,l=0,r=0;
+         while (r<rightSize && l<leftSize) {
+             if (leftArray[l] < rightArray[r]) {
+                 array[i]=leftArray[l];
+                 i++;
+                 l++;
+             }else{
+                 array[i]=rightArray[r];
+                 i++;
+                 r++;
+             }
+         }
+         while (r<rightSize){
+             array[i]=rightArray[r];
+             i++;
+             r++;
+         }
+         while (l<leftSize){
+             array[i]=leftArray[l];
+             i++;
+             l++;
+         }
     }
 
     /**
